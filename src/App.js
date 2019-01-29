bdd246c495f5fc1c35f496a2479edab4e5cb9207
p-mh <pauline.miele@gmail.com>
2019-01-29 10:43:45 +0100

    create grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Grid from './components/Grid';
+
 class App extends Component {
   render() {
-    return <div className="App">App</div>;
+    return (
+      <div className="App">
+        <Grid />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Grid.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import './grid.css';
+
+export default class Grid extends Component {
+  state = {
+    grid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  };
+
+  render() {
+    const grid = this.state.grid.map(line => (
+      <div className="line">
+        {line.map(box => (
+          <div className="box" />
+        ))}
+      </div>
+    ));
+
+    return <div>{grid}</div>;
+  }
+}
